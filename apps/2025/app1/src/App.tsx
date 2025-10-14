721da361c442a08a46d727e416de447518df865c
Omur <omur@erciyes.edu.tr>
2025-10-14 16:01:35 +0300

    app1 final

--- apps/2025/app1/src/App.tsx
+++ apps/2025/app1/src/App.tsx
@@ -1,17 +1,19 @@
 import './App.css'
 import {Counter} from "./counter.tsx";
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(5);
 
     return (
         <>
-            <Counter/>
-            <Counter/>
-            <Counter/>
-            <Counter/>
-            <Counter/>
-            <Counter/>
-
+            <h1>{count}</h1>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
+            <Counter count1={count} setCount1={setCount}/>
         </>
     )
 }
--- apps/2025/app1/src/counter.tsx
+++ apps/2025/app1/src/counter.tsx
@@ -1,15 +1,21 @@
 import {useState} from "react";
 
-export const Counter = () => {
+export const Counter = ({count1, setCount1}) => {
     const [count, setCount] = useState(0)
 
     return <>
             <h1>{count}</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
+                <button onClick={() => {
+                    setCount((count) => count + 1)
+                    setCount1((count1) => count1 + 1)
+                }}>
                     Artır
                 </button>
-                <button onClick={() => setCount((count) => count - 1)}>
+                <button onClick={() => {
+                    setCount((count) => count - 1)
+                    setCount1((count1) => count1 - 1)
+                }}>
                     Azalt
                 </button>
             </div>
