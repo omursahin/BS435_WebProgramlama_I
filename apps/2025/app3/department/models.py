17dcbb6c1e7f15aad219d24d04a1ff6559159c6b
Omur <omur@erciyes.edu.tr>
2025-12-02 15:28:45 +0300

    update

--- apps/2025/app3/department/models.py
+++ apps/2025/app3/department/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+from faculty.models import Faculty
+
+
 # Create your models here.
 class Department(models.Model):
     name = models.CharField(max_length=100)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name
